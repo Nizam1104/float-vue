6aaffd8921ebe69f754e24ef499c4e7c4d2e90aa
Nizam_V <nizamvali1104@gmail.com>
2024-09-08 18:15:22 +0530

    builded files

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,7 @@
 import vue from 'rollup-plugin-vue'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import postcss from 'rollup-plugin-postcss';
 
 export default {
   input: 'src/index.js',
@@ -14,6 +15,7 @@ export default {
       css: true,
       compileTemplate: true
     }),
+    postcss(),
     nodeResolve(),
     commonjs()
   ],
